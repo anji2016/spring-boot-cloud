d68f28a9ce7b21b4270cdef8cba04a40b5b4d870
anji2016 <m.anjaneyulu24@gmail.com>
2024-08-01 17:22:32 +0530

    package restructure changes

--- alpha-data-service/src/main/java/com/example/alphadataservice/controller/AlphaDataServiceController.java
+++ alpha-data-service/src/main/java/com/example/alphadataservice/controller/AlphaDataServiceController.java
@@ -1,4 +1,4 @@
-package com.example.alphadataservice;
+package com.example.alphadataservice.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
--- gamma-data-service/src/main/java/com/example/gammadataservice/GammaDataServiceApplication.java
+++ gamma-data-service/src/main/java/com/example/gammadataservice/GammaDataServiceApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.example.gammadataservice.client", "com.example.gammadataservice.controller"})
 @EnableDiscoveryClient
 @EnableFeignClients
 public class GammaDataServiceApplication {
